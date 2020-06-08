8aac3e4f9ea07509eb2cc2a9d26409897286fc60
Mike Boring <shekinah999@msn.com>
2020-06-08 09:20:09 -0600

    Passing all test

--- babynames.py
+++ babynames.py
@@ -43,16 +43,17 @@ def extract_names(filename):
     the name-rank strings in alphabetical order.
     ['2006', 'Aaliyah 91', 'Aaron 57', 'Abagail 895', ...]
     """
-    print('Filename: ', )
     sorting_names = []
     with open(filename) as f:
-        text = f.read().split()
-        for text_section in text:
-            if text_section.endswith('</h3>'):
-                sorting_names.append(text_section.replace('</h3>', ''))
+        text = f.read()
+        year = r'Popularity\sin\s(\d\d\d\d)'
+        y = re.search(year, text)
+        sorting_names.append(
+            text[y.start():y.end()].replace('Popularity in ', ''))
+        for text_section in text.split():
             if text_section.startswith('align="right"><td>'):
-                sorting_names.append(text_section.replace(
-                    'align="right"><td>', '').replace('</td><td>', ' ').replace('</td>', ''))
+                sorting_names.append(text_section.replace('align="right"><td>', '').replace(
+                    '</td><td>', ' ').replace('</td>', ''))
     final_list = []
     for single_name in sorting_names:
         if single_name == sorting_names[0]:
@@ -60,14 +61,13 @@ def extract_names(filename):
         else:
             split_single_name = single_name.split()
             if len(split_single_name) == 3:
-                if split_single_name[1] in ' '.join(final_list):
-                    continue
-                if split_single_name[2] in ' '.join(final_list):
-                    continue
-                final_list.append(
-                    split_single_name[1] + ' ' + split_single_name[0])
-                final_list.append(
-                    split_single_name[2] + ' ' + split_single_name[0])
+                if split_single_name[1]+' ' not in ' '.join(final_list):
+                    final_list.append(
+                        split_single_name[1] + ' ' + split_single_name[0])
+                if split_single_name[2]+' ' not in ' '.join(final_list):
+                    final_list.append(
+                        split_single_name[2] + ' ' + split_single_name[0])
+                continue
     final_sorted_list = sorted(final_list)
     return final_sorted_list
 
@@ -97,7 +97,6 @@ def main(args):
         sys.exit(1)
 
     file_list = ns.files
-    print('file list: ', type(file_list))
 
     # option flag
     create_summary = ns.summaryfile
@@ -107,10 +106,17 @@ def main(args):
     # Use the create_summary flag to decide whether to print the list
     # or to write the list to a summary file (e.g. `baby1990.html.summary`).
 
-    # +++your code here+++
-    names_list = extract_names(file_list[0])
-    for name in names_list:
-        print(name)
+    if create_summary:
+        for name_list in file_list:
+            with open(name_list + '.summary', 'w') as f:
+                new_list = extract_names(name_list)
+                for name in new_list:
+                    f.write(name + '\n')
+    else:
+        for name_list in file_list:
+            new_list = extract_names(name_list)
+            for name in new_list:
+                print(name)
 
 
 if __name__ == '__main__':
